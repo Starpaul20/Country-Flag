6ec5dccd1c400b247ad6f722c137570c572a1488
PaulBender <paulbender@live.com>
2017-05-14 10:45:22 -0400

    Bump to version v1.2.1

--- inc/plugins/country.php
+++ inc/plugins/country.php
@@ -133,7 +133,7 @@ function country_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.2",
+		"version"			=> "1.2.1",
 		"codename"			=> "country",
 		"compatibility"		=> "18*"
 	);
